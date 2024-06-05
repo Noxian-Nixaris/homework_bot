c61c4463bf0477ff1562450988a31054c96b99ad
Noxian <nixaris@gmail.com>
2024-06-05 23:26:50 +0300

    pep fix

--- homework.py
+++ homework.py
@@ -3,7 +3,6 @@ import os
 import requests
 import time
 
-
 from dotenv import load_dotenv
 from telebot import TeleBot
 
@@ -23,7 +22,6 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
